fb8c7929508d344505893ac3100e5008fe0fda96
liuchenyang0515 <897418643@qq.com>
2020-10-23 09:24:00 +0800

    鸿蒙应用的能力（Ability）及跳转方法

---
+++ step02/entry/src/main/java/com/example/tv_yemiantiaozhuan/MainAbility.java
@@ -0,0 +1,13 @@
+package com.example.tv_yemiantiaozhuan;
+
+import com.example.tv_yemiantiaozhuan.slice.MainAbilitySlice;
+import ohos.aafwk.ability.Ability;
+import ohos.aafwk.content.Intent;
+
+public class MainAbility extends Ability {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setMainRoute(MainAbilitySlice.class.getName());
+    }
+}
---
+++ step02/entry/src/main/java/com/example/tv_yemiantiaozhuan/TV_yemiantiaozhuan.java
@@ -0,0 +1,10 @@
+package com.example.tv_yemiantiaozhuan;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class TV_yemiantiaozhuan extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
